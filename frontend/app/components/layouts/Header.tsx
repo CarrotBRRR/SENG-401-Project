c1248fec6165b52830267705adf98c3e48c28a17
justin-phxm <113923596+justin-phxm@users.noreply.github.com>
2024-02-16 19:40:52 -0700

    add navbar boilerplate

--- frontend/app/components/layouts/Header.tsx
+++ frontend/app/components/layouts/Header.tsx
@@ -1,5 +1,66 @@
-import React from "react";
-
+import {
+  Avatar,
+  Dropdown,
+  DropdownDivider,
+  DropdownHeader,
+  DropdownItem,
+  Navbar,
+  NavbarBrand,
+  NavbarCollapse,
+  NavbarLink,
+  NavbarToggle,
+} from "flowbite-react";
+import Image from "next/image";
 export default function Header() {
-  return <div className=" bg-yellow-200 dark:bg-purple-400">Header</div>;
+  return (
+    <Navbar fluid rounded>
+      <NavbarBrand href="/">
+        <Image
+          height={36}
+          width={36}
+          src="/favicon.svg"
+          className="mr-3 h-6 sm:h-9"
+          alt="ToolShed Logo"
+        />
+        <span className="self-center whitespace-nowrap text-xl font-semibold dark:text-white">
+          ToolShed
+        </span>
+      </NavbarBrand>
+      <div className="flex md:order-2">
+        <Dropdown
+          arrowIcon={false}
+          inline
+          label={
+            <Avatar
+              alt="User settings"
+              img="https://flowbite.com/docs/images/people/profile-picture-5.jpg"
+              rounded
+            />
+          }
+        >
+          <DropdownHeader>
+            <span className="block text-sm">Bonnie Green</span>
+            <span className="block truncate text-sm font-medium">
+              name@flowbite.com
+            </span>
+          </DropdownHeader>
+          <DropdownItem>Dashboard</DropdownItem>
+          <DropdownItem>Settings</DropdownItem>
+          <DropdownItem>Earnings</DropdownItem>
+          <DropdownDivider />
+          <DropdownItem>Sign out</DropdownItem>
+        </Dropdown>
+        <NavbarToggle />
+      </div>
+      <NavbarCollapse>
+        <NavbarLink href="#" active>
+          Home
+        </NavbarLink>
+        <NavbarLink href="#">About</NavbarLink>
+        <NavbarLink href="#">Services</NavbarLink>
+        <NavbarLink href="#">Pricing</NavbarLink>
+        <NavbarLink href="#">Contact</NavbarLink>
+      </NavbarCollapse>
+    </Navbar>
+  );
 }
