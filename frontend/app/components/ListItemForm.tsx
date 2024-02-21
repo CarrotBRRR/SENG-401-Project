1393921c20681ca794bc3421abee7f1967177e16
justin-phxm <113923596+justin-phxm@users.noreply.github.com>
2024-02-21 14:44:45 -0700

    Add ListingLocation component to ListItemForm

--- frontend/app/components/ListItemForm.tsx
+++ frontend/app/components/ListItemForm.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 import ListingDetails from "./listItemFormComponents/ListingDetails";
 import ListingMedia from "./listItemFormComponents/ListingMedia";
+import ListingLocation from "./listItemFormComponents/ListingLocation";
 
 export default function ListItemForm() {
   return (
     <div className="text-brand gap-2 flex flex-col">
       <ListingDetails></ListingDetails>
       <ListingMedia></ListingMedia>
+      <ListingLocation></ListingLocation>
     </div>
   );
 }
