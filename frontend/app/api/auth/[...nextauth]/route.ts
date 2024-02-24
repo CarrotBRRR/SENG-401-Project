54002e5bf2f66670ef3d37d013a3dbb53ceac889
justin-phxm <113923596+justin-phxm@users.noreply.github.com>
2024-02-23 17:30:59 -0700

    refactor auth

--- frontend/app/api/auth/[...nextauth]/route.ts
+++ frontend/app/api/auth/[...nextauth]/route.ts
@@ -1,13 +1,6 @@
-import NextAuth, { AuthOptions } from "next-auth";
-import GoogleProvider from "next-auth/providers/google";
-export const authOptions = {
-  providers: [
-    GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID as string,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
-    }),
-  ],
-};
-export const handler = NextAuth(authOptions);
+import { authOptions } from "@/app/utils/authOptions";
+import NextAuth from "next-auth/next";
+
+const handler = NextAuth(authOptions);
 
 export { handler as GET, handler as POST };
