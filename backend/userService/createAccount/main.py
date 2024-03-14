23581f0f99834d5e52417b5a6c3f6c1c99da6811
HoussemZ4 <houssem.zaggar@ucalgary.ca>
2024-03-14 14:26:06 -0600

    profile pic

--- backend/userService/createAccount/main.py
+++ backend/userService/createAccount/main.py
@@ -2,7 +2,68 @@ import json
 import boto3
 import uuid
 import traceback
+import base64
+import hashlib
+from decimal import Decimal
+import time
+import requests
 
+def sort_dict(dict, exclude):
+    myKeys = list(dict.keys())
+    myKeys.sort()
+    for i in range(len(exclude)):
+        if exclude[i] in myKeys:
+            myKeys.remove(exclude[i])
+
+    return {i: dict[i] for i in myKeys}
+
+def create_query_string(dict):
+    query_string = ""
+    for ind, (key, value) in enumerate(dict.items()):
+        query_string = f"{key}={value}" if ind == 0 else f"{query_string}&{key}={value}"
+    return query_string
+
+def create_signature(body, api_secret):
+    exclude = ["api_key", "resource_type", "cloud_name"]
+    sorted_body = sort_dict(body, exclude)
+    query_string = create_query_string(sorted_body)
+    query_string = f"{query_string}{api_secret}"
+    hashed = hashlib.sha1(query_string.encode("utf-8")).hexdigest()
+    return hashed
+
+def post_to_cloudinary(file, timestamp):
+    ssm = boto3.client('ssm')
+    parameter_names = []
+    key_string = ssm.get_parameter(
+        Name="CloudinaryKey",
+        WithDecryption=True
+    )["Parameter"]["Value"]
+
+    keys = key_string.split(",")
+
+    cloud_name = keys[0]
+    api_key = keys[1]
+    api_secret = keys[2]
+
+    # Set up the URL
+    url = f'https://api.cloudinary.com/v1_1/{cloud_name}/image/upload/'
+
+    # Set up the payload
+    payload = {
+        'api_key': api_key,
+        'timestamp': timestamp
+    }
+    file = {
+        'file': file
+    }
+
+    # Create a signature and add it to the payload
+    payload["signature"] = create_signature(payload, api_secret)
+
+    # Post the image to Cloudinary
+    res = requests.post(url, files=file, data=payload)
+    print(res.json())
+    return res.json()
 
 def handler(event, context, table=None):
     # pass in table for testing
@@ -34,6 +95,27 @@ def handler(event, context, table=None):
             "bio": data["bio"],
             "location": data["location"]
             }
+        # check if request contains profile pic
+        if data.get("image") is not None and data.get("image") != "null": 
+            profile_pic = data.get("image")
+            item["profilePicture"] = profile_pic
+
+            # decode image and hash it 
+            image_bytes = base64.b64decode(profile_pic)
+            image_hash = hashlib.sha256(image_bytes).hexdigest()
+
+            item["imageHash"] = image_hash
+
+            # Save the image to a temp file
+            filename = "/tmp/img.png"
+
+            with open(filename, "wb") as f:
+                f.write(image_bytes)
+            
+            with open(filename, "rb") as f:
+                post_to_cloudinary(f, Decimal(time.time()))
+
+            
         table.put_item(Item=item)
         return {
             "statusCode": 200,
@@ -55,6 +137,3 @@ def handler(event, context, table=None):
                     "message": str(e),
                     "stack_trace": traceback.format_exc()
                 })}
-
-
-
